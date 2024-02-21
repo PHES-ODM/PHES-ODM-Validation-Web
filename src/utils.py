976f4941aba4e24d28f9a18db1f51790e62d9c6e
zargot <zargot.ripley@gmail.com>
2024-02-21 12:05:41 -0500

    utils: fix html list gen

--- src/utils.py
+++ src/utils.py
@@ -58,10 +58,14 @@ def get_validation_id(pathname: str) -> Tuple[str, str]:
 
 def gen_html_list(xs: Union[list, dict]) -> Component:
     if isinstance(xs, list):
-        return html.Ul(list(map(html.Li, xs)), className='compact-list')
+        return html.Ul(list(map(gen_html_list, xs)), className='compact-list')
     elif isinstance(xs, dict):
         items: List[html.Li] = []
-        for key, values in xs.items():
-            items.append(html.Li([key, gen_html_list(values)]))
+        for key, val in xs.items():
+            if not (isinstance(val, dict) or isinstance(val, list)):
+                items.append(html.Li(f'{key}: {val}'))
+            else:
+                items.append(html.Li([key, gen_html_list(val)]))
         return html.Ul(items)
-    assert False
+    else:
+        return html.Li(xs)
